01c9a5e52989cf0a34b97b6e2ed13d94289f6d8f
DwrUisce <105501146+DwrUisce@users.noreply.github.com>
2023-01-27 15:57:16 +0000

    v1

---
+++ data.js
@@ -0,0 +1,206 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-cwrt-t-mawr-wybrnant",
+      "name": "Cwrt Tŷ Mawr Wybrnant",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.3180863026797986,
+        "pitch": 0.012114675341839742,
+        "fov": 1.0892371086343458
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8923234716212534,
+          "pitch": -0.20787397204109226,
+          "rotation": 0,
+          "target": "2-y-mewnlif-or-afon"
+        },
+        {
+          "yaw": -0.15675556163451354,
+          "pitch": 0.1475428703833188,
+          "rotation": 0,
+          "target": "1-y-pwerdy"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.136023941337461,
+          "pitch": -0.4502131701951342,
+          "title": "<span lang=\"CY\" style=\"font-size:11.0pt;line-height:\n107%;font-family:&quot;Calibri&quot;,sans-serif;mso-fareast-font-family:Calibri;\nmso-bidi-font-family:&quot;Times New Roman&quot;;mso-ansi-language:CY;mso-fareast-language:\nEN-US;mso-bidi-language:AR-SA\">Neges o Groeso</span>",
+          "text": "<p class=\"MsoNormal\"><span lang=\"CY\">Croeso i daith rithiol\no amgylch system tyrbin dŵr Tŷ Mawr Wybrnant. Mae'r system hon yn ffordd o\narddangos y potensial i gynhyrchu trydan dŵr ar gyfer hunan-ddefnydd a hynny\nmewn modd glân a chost-effeithiol.</span><span lang=\"EN-US\"><o:p></o:p></span></p>\n\n<p class=\"MsoNormal\"><span lang=\"CY\">Mae’r project hwn yn gydweithrediad\nrhwng Prifysgol Bangor, Coleg y Drindod Dulyn, a’r Ymddiriedolaeth\nGenedlaethol. Fe’i gwnaed yn bosibl oherwydd cymorth gan Raglen Interreg ERDF\nCymru-Iwerddon 2014-2020, drwy’r Project Dŵr Uisce.</span><span lang=\"EN-US\"><o:p></o:p></span></p><p class=\"MsoNormal\"><span lang=\"CY\"><br></span></p><p class=\"MsoNormal\"><span lang=\"CY\"></span></p><p class=\"MsoNormal\"><span lang=\"CY\">I fwrw ymlaen â'r daith, ewch i'r\npwerdy neu at y mewnlif o’r afon trwy glicio ar yr arwyddion ^.&nbsp;</span><span lang=\"EN-US\"><o:p></o:p></span></p><p class=\"MsoNormal\"><span lang=\"CY\"><br></span></p>"
+        },
+        {
+          "yaw": -3.0938749273763086,
+          "pitch": -0.05893037784843358,
+          "title": "<span style=\"font-size: 13px; background-color: rgba(58, 68, 84, 0.8);\">Tŷ Mawr Wybrnant</span>",
+          "text": "<p class=\"MsoNormal\"><span lang=\"CY\">Mae ffermdy hanesyddol Tŷ Mawr\nWybrnant, a adeiladwyd yn yr 16eg ganrif, yn atyniad twristaidd adnabyddus a\nchaiff ei reoli gan yr Ymddiriedolaeth Genedlaethol. Mewn ymdrech i ddefnyddio\nllai o ynni i oleuo a gwresogi'r adeilad, gosodwyd system ynni dŵr bychan i\ngynhyrchu tua 4 cilowat gan ddefnyddio dŵr afon Wybrnant gerllaw. Mae dŵr yn\ncael ei dynnu o'r afon ar dir uwch (+35m) ac yna'n cael ei gludo trwy bibell\n300m o hyd at y pwerdy. Ar ôl cynhyrchu trydan yn y tyrbin, mae'r dŵr yn\nailymuno â'r nant gerllaw’r ffermdy hanesyddol.</span><span lang=\"EN-US\"><o:p></o:p></span></p>"
+        },
+        {
+          "yaw": -2.0015153822004645,
+          "pitch": 0.15787664280461833,
+          "title": "<p class=\"MsoListParagraph\" style=\"text-indent:-18.0pt;mso-list:l0 level1 lfo1\"><!--[if !supportLists]--><span lang=\"EN-US\">1.<span style=\"font-variant-numeric: normal; font-variant-east-asian: normal; font-stretch: normal; font-size: 7pt; line-height: normal; font-family: &quot;Times New Roman&quot;;\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\n</span></span><!--[endif]--><span lang=\"CY\">Y mewnlif o’r afon</span><span lang=\"EN-US\"><o:p></o:p></span></p>",
+          "text": "<span lang=\"CY\" style=\"font-size:11.0pt;line-height:\n107%;font-family:&quot;Calibri&quot;,sans-serif;mso-fareast-font-family:Calibri;\nmso-bidi-font-family:&quot;Times New Roman&quot;;mso-ansi-language:CY;mso-fareast-language:\nEN-US;mso-bidi-language:AR-SA\">Afon Wybrnant a ddefnyddir i gyflenwi dŵr i'r\ntyrbin yn y Pwerdy er mwyn cynhyrchu trydan.</span>"
+        },
+        {
+          "yaw": -1.343120975541897,
+          "pitch": -0.05295053836865904,
+          "title": "<span style=\"font-size: 13px; background-color: rgba(58, 68, 84, 0.8);\">Tŷ’r Gofalwr</span>",
+          "text": "<p class=\"MsoNormal\"><span lang=\"CY\">&nbsp;Llety ar gyfer gofalwr a\nbenodwyd gan yr Ymddiriedolaeth Genedlaethol.</span><span lang=\"EN-US\"><o:p></o:p></span></p>"
+        }
+      ]
+    },
+    {
+      "id": "1-y-pwerdy",
+      "name": "Y Pwerdy",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.4092035959244811,
+        "pitch": 0.1830338196033896,
+        "fov": 1.152956624339207
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2666355612363702,
+          "pitch": -0.21160666259306993,
+          "rotation": 0,
+          "target": "2-y-mewnlif-or-afon"
+        },
+        {
+          "yaw": -2.712556376328319,
+          "pitch": -0.0805984012888139,
+          "rotation": 0,
+          "target": "0-cwrt-t-mawr-wybrnant"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.03197401553895318,
+          "pitch": 0.2826402033239894,
+          "title": "Y Pwerdy",
+          "text": "<span lang=\"CY\" style=\"font-size:11.0pt;line-height:\n107%;font-family:&quot;Calibri&quot;,sans-serif;mso-fareast-font-family:Calibri;\nmso-bidi-font-family:&quot;Times New Roman&quot;;mso-ansi-language:CY;mso-fareast-language:\nEN-US;mso-bidi-language:AR-SA\">Gwnaed y pwerdy â ffrâm ddur wedi’i gorchuddio â\nphren lleol, er mwyn cysgodi’r tyrbin a’r offer trydanol/rheoli.&nbsp;</span>"
+        },
+        {
+          "yaw": 1.0101399752047495,
+          "pitch": 0.16921244991049278,
+          "title": "Tyrbin",
+          "text": "<span style=\"font-family: Calibri, sans-serif; font-size: 14.6667px;\">Pwmp dŵr confensiynol yw’r tyrbin sydd wedi'i addasu i weithredu’r ffordd groes fel tyrbin, gydag uchafswm allbwn pŵer o tua 4 cilowat.</span><div><font face=\"Calibri, sans-serif\"><span style=\"font-size: 14.6667px;\"><br></span></font><div><span lang=\"CY\" style=\"font-size:11.0pt;line-height:\n107%;font-family:&quot;Calibri&quot;,sans-serif;mso-fareast-font-family:Calibri;\nmso-bidi-font-family:&quot;Times New Roman&quot;;mso-ansi-language:CY;mso-fareast-language:\nEN-US;mso-bidi-language:AR-SA\">Gallwch fwrw ymlaen â’r daith drwy glicio ar yr\narwydd ^ ar y sied i fynd yn eich blaen at y mewnlif o’r afon, neu cliciwch ar\nyr ail arwydd ^ er mwyn dychwelyd i’r prif gwrt</span><font face=\"Calibri, sans-serif\"><span style=\"font-size: 14.6667px;\"><br></span></font><div><span style=\"font-family: Calibri, sans-serif; font-size: 14.6667px;\"><br></span></div></div></div>"
+        },
+        {
+          "yaw": -2.305900716590891,
+          "pitch": -0.10547943308875318,
+          "title": "<span style=\"font-family: Calibri, sans-serif; font-size: 14.6667px; background-color: rgba(58, 68, 84, 0.8);\">Tŷ’r Gofalwr</span>",
+          "text": "<div>Dyma<span style=\"font-family: Calibri, sans-serif; font-size: 11pt;\">'r&nbsp;</span>d<span style=\"font-family: Calibri, sans-serif; font-size: 11pt;\">ŷ</span></div>"
+        }
+      ]
+    },
+    {
+      "id": "2-y-mewnlif-or-afon",
+      "name": "Y mewnlif o’r afon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.17969992117443567,
+        "pitch": 0.47323136267955945,
+        "fov": 1.0669052943922732
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0987058594804697,
+          "pitch": -0.05424416649488428,
+          "rotation": 0,
+          "target": "0-cwrt-t-mawr-wybrnant"
+        },
+        {
+          "yaw": -2.521032085706601,
+          "pitch": -0.05085770991978045,
+          "rotation": 0,
+          "target": "2-y-mewnlif-or-afon"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.09297549591162912,
+          "pitch": 0.4988988767657627,
+          "title": "Sgrin a chymeriant",
+          "text": "<span lang=\"CY\" style=\"font-size:11.0pt;line-height:\n107%;font-family:&quot;Calibri&quot;,sans-serif;mso-fareast-font-family:Calibri;\nmso-bidi-font-family:&quot;Times New Roman&quot;;mso-ansi-language:CY;mso-fareast-language:\nEN-US;mso-bidi-language:AR-SA\">Os oes llif dŵr digonol yn y nant, bydd rhan\nohono'n llifo trwy'r sgrin fetel ac yna i mewn i'r bibell 300m o hyd sy'n\narwain at y pwerdy</span>"
+        },
+        {
+          "yaw": -0.3816850261912048,
+          "pitch": 0.35183955826013147,
+          "title": "Gored",
+          "text": "<span lang=\"CY\" style=\"font-size:11.0pt;line-height:\n107%;font-family:&quot;Calibri&quot;,sans-serif;mso-fareast-font-family:Calibri;\nmso-bidi-font-family:&quot;Times New Roman&quot;;mso-ansi-language:CY;mso-fareast-language:\nEN-US;mso-bidi-language:AR-SA\">Mae'r dŵr yn cael ei dynnu o’r afon trwy gored\nfechan sy'n codi lefel y dŵr rhyw ychydig ac sy'n cynnwys rhicyn i ganiatáu i'r\nLlif Di-gyffwrdd basio trwy’r nant yn annibynnol ar y gweithrediadau hydro</span>"
+        },
+        {
+          "yaw": -2.286566312918575,
+          "pitch": 0.10587187588694214,
+          "title": "Diolch",
+          "text": "<span lang=\"CY\" style=\"font-size:11.0pt;line-height:\n107%;font-family:&quot;Calibri&quot;,sans-serif;mso-fareast-font-family:Calibri;\nmso-bidi-font-family:&quot;Times New Roman&quot;;mso-ansi-language:CY;mso-fareast-language:\nEN-US;mso-bidi-language:AR-SA\">Ewch ymlaen â'r daith trwy glicio ar yr arwydd ^\ni ddychwelyd i'r prif gwrt</span>"
+        }
+      ]
+    }
+  ],
+  "name": "Tŷ Mawr Wybrnant",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
